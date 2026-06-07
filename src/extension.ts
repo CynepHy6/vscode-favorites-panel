f7c7df51b5cff9e123536db30a69c7d7601c179e
andrey.v <andrey.voronkov@skyeng.ru>
2026-06-07 18:28:44 +0300

    rename package

--- src/extension.ts
+++ src/extension.ts
@@ -135,7 +135,7 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.window.registerTreeDataProvider('favoritesPanelExplorer', favoritesPanelProvider);
     vscode.commands.registerCommand(`${PLUGIN_NAME}.refreshPanel`, () => favoritesPanelProvider.refresh());
     vscode.commands.registerCommand(`${PLUGIN_NAME}.openFavoritesPanelSettings`, () => {
-        runCommand(['workbench.action.openSettings', `@ext:hy6.favorites-panel`]);
+        runCommand(['workbench.action.openSettings', `@ext:hy6.favorites-panel-fork`]);
     }),
         vscode.commands.registerCommand(`${PLUGIN_NAME}.openUserJsonSettings`, () => {
             runCommand(['workbench.action.openSettingsJson']);
